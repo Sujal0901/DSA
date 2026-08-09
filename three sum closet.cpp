4a8354754c1cc5a53c5e4499b7fd6f164f43dcaf
Sujal0901 <sujalrai452@gmail.com>
2026-08-09 14:52:05 +0530

    seven commit

---
+++ three sum closet.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<cmath>
+using namespace std;
+
+class Solution {
+public:
+    int threeSumClosest(vector<int>& nums, int target) {
+        sort(nums.begin(),nums.end());
+        int ans = nums[0]+nums[1]+nums[2];
+        for( int i = 0 ; i< nums.size()-2;i++){
+             int left = i+1;
+             int right = nums.size()-1;
+              while(left<right){
+                 int sum = nums[i]+nums[left]+nums[right];
+                 if(abs(sum-target)< abs(ans - target))
+                 ans = sum;
+                 if(sum<target)
+                 left++;
+                 else
+                 right--;
+              }
+        }
+              return ans;
+        }
+        
+    
+};
+int main(){
+    Solution sol;
+    vector<int> nums = {-1,2,1,-4};
+    int target = 1;
+    int result = sol.threeSumClosest(nums,target);
+    cout<<result;
+}
