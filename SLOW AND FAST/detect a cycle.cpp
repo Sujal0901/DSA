89a73cbedd5b90ecf1f48d50eaa718ecaa72263a
Sujal0901 <sujalrai452@gmail.com>
2026-08-26 21:33:15 +0530

    slow and fast

---
+++ SLOW AND FAST/detect a cycle.cpp
@@ -0,0 +1,56 @@
+# include<iostream>
+using namespace std;
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+
+
+
+struct ListNode {
+    int val;
+    ListNode* next;
+
+    ListNode(int x) {
+        val = x;
+        next = NULL;
+    }
+};
+class Solution{
+    public:
+     bool hasCycle(ListNode *head) {
+        ListNode*slow = head;
+        ListNode*fast = head;
+        while(fast!= NULL && fast->next!= NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+            if(slow==fast)
+            return true;
+        }
+        return false;
+    }
+};
+int main(){
+      // Create nodes
+    ListNode* head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(3);
+    head->next->next->next = new ListNode(4);
+
+    // Create cycle:
+    // 4 -> 2
+    head->next->next->next->next = head->next;
+
+    Solution obj;
+
+    if (obj.hasCycle(head))
+        cout << "Cycle exists";
+    else
+        cout << "No cycle";
+
+    return 0;
+}
